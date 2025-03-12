6cf6f58e4a7c9f46c2ce7b1cad27c1e076b46682
Nabil <nabilrajankanz@gmail.comm>
2025-03-12 21:42:07 +0700

    [NR] feat: add splash screen video

--- lib/features/splash_screen/page/splash_screen.dart
+++ lib/features/splash_screen/page/splash_screen.dart
@@ -1,49 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:mycareer_new/features/on_boarding/page/on_boarding.dart';
-
-import '../../../core/theme/color_value.dart';
+import 'package:flutter/services.dart';
+import 'package:mycareer_new/features/home/page/home.dart';
+import 'package:video_player/video_player.dart';
 
 class SplashScreenPage extends StatefulWidget {
-  const SplashScreenPage({Key? key}) : super(key: key);
-
   @override
-  State<SplashScreenPage> createState() => _SplashScreenPageState();
+  _SplashScreenPageState createState() => _SplashScreenPageState();
 }
 
 class _SplashScreenPageState extends State<SplashScreenPage> {
+  late VideoPlayerController _controller;
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const OnBoardingPage(),
-          ));
+    _controller = VideoPlayerController.asset("assets/videos/splash.mp4")
+      ..initialize().then((_) {
+        debugPrint("Video berhasil diinisialisasi");
+        setState(() {});
+        _controller.play();
+      }).catchError((error) {
+        debugPrint("Error inisialisasi video: $error");
+      }); // Putar video langsung
+    _controller.setLooping(false); // Tidak mengulang
+    _controller.addListener(() {
+      if (_controller.value.position == _controller.value.duration) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      }
     });
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light, // Icon status bar gelap
+      ),
+    );
+
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 185.h,
-              width: 185.w,
-
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                shape: BoxShape.circle,
-              ),
-            ),
-
-          ],
-        ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          _controller.value.isInitialized
+              ? VideoPlayer(_controller)
+              : Center(child: CircularProgressIndicator()),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      child: CareerHubPage(),
+      child: SplashScreenPage(),
     );
   }
 }
